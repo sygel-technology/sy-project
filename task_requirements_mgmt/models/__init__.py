6b1e0cfb3b90556321b624a5a6791bfbb6b54a0d
angel <angel.garcia@sygel.com>
2024-04-10 12:55:55 +0200

    [NEW] task_requirements_mgmt: Manage requirements related to a task.

---
+++ task_requirements_mgmt/models/__init__.py
@@ -0,0 +1,5 @@
+# Copyright 2021 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from . import task_requirement
+from . import project_task
