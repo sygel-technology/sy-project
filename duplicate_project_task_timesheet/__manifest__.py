8695c315a8e2b356dcc132776e1c28b9d1fb24c7
angel <angel.garcia@sygel.com>
2024-04-10 18:01:37 +0200

    [NEW] duplicate_project_task_timesheet: Button to duplicate project task timesheet lines.

---
+++ duplicate_project_task_timesheet/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Duplicate Project Task Timesheet",
+    "summary": "Button to duplicate project task timesheet lines",
+    "version": "15.0.1.0.0",
+    "category": "Timesheets",
+    "website": "https://www.sygel.es",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'hr_timesheet',
+    ],
+    "data": [
+        'views/project_views.xml'
+    ],
+}
