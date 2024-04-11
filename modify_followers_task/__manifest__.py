109856c7282009e09f34d1c3c52d59ac76ec94ef
angel <angel.garcia@sygel.com>
2024-04-11 10:04:07 +0200

    [ADD] modify_followers_task: Modify list of followers from a project task.

---
+++ modify_followers_task/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019 Joan Segui <joan.segui@qubiq.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Modify Followers Task",
+    "summary": "Modify list of followers from a project task",
+    "version": "15.0.1.0.0",
+    "category": "Project",
+    "website": "https://www.qubiq.es",
+    "author": "QubiQ, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        'project'
+    ],
+    "data": [
+        'security/ir.model.access.csv',
+        'wizard/modify_followers_task_wizard.xml',
+        'views/project_task_view.xml',
+    ],
+}
