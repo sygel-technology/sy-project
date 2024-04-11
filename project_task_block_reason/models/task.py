fd41b7e43289d85ed812e8722ed3467c88b9454b
angel <angel.garcia@sygel.com>
2024-04-11 18:16:18 +0200

    [ADD] project_task_block_reason: Introduce a reason when a task or ticket is blocked.

---
+++ project_task_block_reason/models/task.py
@@ -0,0 +1,12 @@
+# Copyright 2021 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class Task(models.Model):
+    _inherit = "project.task"
+
+    task_blocked_reason = fields.Text(
+        string="Blocked Reason"
+    )
