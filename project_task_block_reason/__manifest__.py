2206b644846884173dfca6b548828ed405a54ddf
angel <angel.garcia@sygel.com>
2024-04-11 18:16:18 +0200

    [ADD] project_task_block_reason: Introduce a reason when a task or ticket is blocked.

---
+++ project_task_block_reason/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Project Task Block Reason",
+    "summary": "Introduce a reason when a task is blocked",
+    "version": "15.0.1.0.0",
+    "category": "Project",
+    "website": "https://www.sygel.es",
+    "author": "Sygel",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "project",
+    ],
+    "data": [
+        "views/project_views.xml",
+    ],
+}
