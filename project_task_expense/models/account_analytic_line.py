ddf98adbe8fddeb54b57cb9a6ac20c2ffc0258ff
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 10:43:26 +0200

    [T-5944][IMP]project_stock_portal,project_task_expense,project_task_sign,project_task_stage_portal_user_visibility:pre-commit auto fixes

--- project_task_expense/models/account_analytic_line.py
+++ project_task_expense/models/account_analytic_line.py
@@ -1,16 +1,14 @@
 # Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class AccountAnalyticLine(models.Model):
     _inherit = "account.analytic.line"
 
     expense_id = fields.Many2one(
-        string="Expense",
-        related="move_line_id.expense_id",
-        store=True
+        string="Expense", related="move_line_id.expense_id", store=True
     )
 
     def _timesheet_postprocess_values(self, values):
@@ -20,4 +18,3 @@ class AccountAnalyticLine(models.Model):
         ).ids:
             res[key].pop("amount", None)
         return res
-        
