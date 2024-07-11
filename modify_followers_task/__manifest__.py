4decde2907d7802208d2a33e13507ab147890270
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 10:47:08 +0200

    [T-5944][IMP]duplicate_project_task_timesheet,modify_followers_task,project_task_block_reason,task_requirements_mgmt:pre-commit auto fixes

--- modify_followers_task/__manifest__.py
+++ modify_followers_task/__manifest__.py
@@ -6,17 +6,15 @@
     "summary": "Modify list of followers from a project task",
     "version": "15.0.1.0.0",
     "category": "Project",
-    "website": "https://www.qubiq.es",
+    "website": "https://github.com/sygel-technology/sy-project",
     "author": "QubiQ, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": [
-        'project'
-    ],
+    "depends": ["project"],
     "data": [
-        'security/ir.model.access.csv',
-        'wizard/modify_followers_task_wizard.xml',
-        'views/project_task_view.xml',
+        "security/ir.model.access.csv",
+        "wizard/modify_followers_task_wizard.xml",
+        "views/project_task_view.xml",
     ],
 }
