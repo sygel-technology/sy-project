29be7ad4ae2691a0fb639dd3468020b216ab1da5
angel <angel.garcia@sygel.es>
2025-03-04 13:50:22 +0100

    [T-7029][MIG] project_task_show_project_manager: Migration to 16.0

--- project_task_show_project_manager/__manifest__.py
+++ project_task_show_project_manager/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Task Show Project Manager",
     "summary": "Show Project Manager in Project Task",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
