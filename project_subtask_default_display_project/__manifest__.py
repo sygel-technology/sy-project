a13945ce8f05d0f05488e0edd7a6b9f5fce1090e
angel <angel.garcia@sygel.es>
2025-03-12 12:49:35 +0100

    [T-7029][MIG] project_subtask_default_display_project: Migration to 16.0

--- project_subtask_default_display_project/__manifest__.py
+++ project_subtask_default_display_project/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Subtask Default Display Project",
     "summary": "Project Subtask Default Display Project",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.1.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
