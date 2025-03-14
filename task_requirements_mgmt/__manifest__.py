e47e4c603bad562faadfa89ee14e8334c77802af
angel <angel.garcia@sygel.es>
2025-03-14 08:33:47 +0100

    [T-7029][MIG] task_requirements_mgmt: Migration to 16.0

--- task_requirements_mgmt/__manifest__.py
+++ task_requirements_mgmt/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Task Requirements Management",
     "summary": "Manage requirements related to a task",
-    "version": "15.0.1.1.0",
+    "version": "16.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel",
