87d5631aa24fb9821462f9edccaaae7d3b6e5150
sygel <alberto.martinez@sygel.es>
2025-11-21 12:17:35 +0100

    [MIG] project_task_description_template_section: Migration to 18.0

--- project_task_description_template_section/__manifest__.py
+++ project_task_description_template_section/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Project Task Description Template Section",
     "summary": "Project Task Description Template Section",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
