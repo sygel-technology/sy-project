e3dd47b2f648a25dfb9619f3bcc2474349394164
sygel <alberto.martinez@sygel.es>
2025-12-12 14:53:36 +0100

    [MIG] duplicate_project_task_timesheet: Migration to 18.0

--- duplicate_project_task_timesheet/__manifest__.py
+++ duplicate_project_task_timesheet/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Duplicate Project Task Timesheet",
     "summary": "Button to duplicate project task timesheet lines",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Timesheets",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel",
