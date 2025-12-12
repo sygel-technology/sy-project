20879099c0c4b72808c1b4e1b86a6125bf3ec5f2
sygel <alberto.martinez@sygel.es>
2025-12-12 15:26:07 +0100

    [MIG] project_odoo_version: Migration to 18.0

--- project_odoo_version/__manifest__.py
+++ project_odoo_version/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Odoo Version",
     "summary": "Project Odoo Version",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Porject",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
