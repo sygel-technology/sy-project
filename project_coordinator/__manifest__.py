d23bf7e8ca30ec04bea718e2ce7d2b70ba45c2a0
sygel <alberto.martinez@sygel.es>
2025-12-12 15:07:06 +0100

    [MIG] project_coordinator: Migration to 18.0

--- project_coordinator/__manifest__.py
+++ project_coordinator/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Coordinator",
     "summary": "Project Coordinator",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
