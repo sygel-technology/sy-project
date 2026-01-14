84ba4deadbdeed5d5ee8121044023fecc406ad21
sygel <alberto.martinez@sygel.es>
2026-01-14 12:40:45 +0100

    [MIG] project_task_subtask_inline: Migration to 18.0

--- project_task_subtask_inline/__manifest__.py
+++ project_task_subtask_inline/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Project Task Subtask Inline",
     "summary": "Project Task Subtask Inline",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
     "author": "Sygel, Odoo Community Association (OCA)",
