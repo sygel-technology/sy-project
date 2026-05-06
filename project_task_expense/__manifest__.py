0726c4e8db64b2ff30064aa002e9a967c51d83fe
sygel <alberto.martinez@sygel.es>
2026-05-06 16:14:07 +0200

    [IMP] project_task_expense: pre-commit auto fixes

--- project_task_expense/__manifest__.py
+++ project_task_expense/__manifest__.py
@@ -7,7 +7,7 @@
     "version": "16.0.1.0.0",
     "category": "Project",
     "website": "https://github.com/sygel-technology/sy-project",
-    "author": "Sygel, Odoo Community Association (OCA)",
+    "author": "Sygel",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
